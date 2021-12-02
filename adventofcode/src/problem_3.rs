be16aadb04c0d7f790b4b0f3141fa8463b0c5533
Michael Chermside <mcherm@mcherm.com>
2021-12-02 09:56:49 -0500

    First fully working version of problem 3.

--- adventofcode/src/problem_3.rs
+++ adventofcode/src/problem_3.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Error, ErrorKind};
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::fmt;
 
 
 #[derive(Debug)]
@@ -15,6 +16,18 @@ fn invalid_movement_error() -> Result<Movement, Error> {
     Err(Error::new(ErrorKind::InvalidData, "Invalid Movement"))
 }
 
+pub enum MovementError {
+    FlyingSubError,
+}
+
+impl fmt::Display for MovementError {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            MovementError::FlyingSubError => write!(f, "FlyingSubError")
+        }
+    }
+}
 
 fn parse_movement(s: &str) -> Result<Movement, Error> {
     lazy_static! {
@@ -71,12 +84,43 @@ fn read_file_of_movements() -> Result<Vec<Movement>, Error>  {
 }
 
 
+#[derive(Debug)]
+struct Position {
+    x: isize,
+    depth: isize,
+}
+
+impl Position {
+    fn move_by(&self, movement: Movement) -> Result<Position, MovementError> {
+        match movement {
+            Movement::Forward(x) => Ok(Position{x: self.x + x, depth: self.depth}),
+            Movement::Down(x) => Ok(Position{x: self.x, depth: self.depth + x}),
+            Movement::Up(x) => if self.depth - x >= 0 {
+                Ok(Position{x: self.x, depth: self.depth - x})
+            } else {
+                Err(MovementError::FlyingSubError)
+            }
+        }
+    }
+}
+
+
+fn update_position(movements: Vec<Movement>, start: Position) -> Result<Position, MovementError> {
+    let mut position = start;
+    for movement in movements {
+        position = position.move_by(movement)?
+    }
+    Ok(position)
+}
+
 
 pub fn main() {
     match read_file_of_movements() {
         Ok(movements) => {
-            for movement in movements {
-                println!("Movement: {:#?}", movement);
+            let end_position = update_position(movements, Position{x: 0, depth: 0});
+            match end_position {
+                Ok(p) => println!("It ends up at: {:#?}", p),
+                Err(err) => println!("Error: {}", err),
             }
         },
         Err(err) => println!("Error: {}", err)
