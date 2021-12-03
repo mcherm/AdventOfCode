6f02bc4eb73dc9a96103a7de70c15dcb837f8d3a
Michael Chermside <mcherm@mcherm.com>
2021-12-03 08:55:08 -0500

    Completed problem 5.

--- adventofcode/src/main.rs
+++ adventofcode/src/main.rs
@@ -1,9 +1,17 @@
-// mod problem_1;
-// mod problem_2;
-//mod problem_3;
+mod problem_1;
+mod problem_2;
+mod problem_3;
 mod problem_4;
+mod problem_5;
 
 
 fn main() {
-    problem_4::main();
+    match 5 {
+        1 => problem_1::main(),
+        2 => problem_2::main(),
+        3 => problem_3::main(),
+        4 => problem_4::main(),
+        5 => problem_5::main(),
+        _ => panic!("Not a valid problem.")
+    }
 }
