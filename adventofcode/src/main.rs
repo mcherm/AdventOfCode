d3db182244613a8a4e25555d52f85d6890239118
Michael Chermside <mcherm@mcherm.com>
2021-12-04 11:24:58 -0500

    Solved #8.

--- adventofcode/src/main.rs
+++ adventofcode/src/main.rs
@@ -5,10 +5,11 @@ mod problem_4;
 mod problem_5;
 mod problem_6;
 mod problem_7;
+mod problem_8;
 
 
 fn main() {
-    match 7 {
+    match 8 {
         1 => problem_1::main(),
         2 => problem_2::main(),
         3 => problem_3::main(),
@@ -16,6 +17,7 @@ fn main() {
         5 => problem_5::main(),
         6 => problem_6::main(),
         7 => problem_7::main(),
+        8 => problem_8::main(),
         _ => panic!("Not a valid problem.")
     }
 }
