dfd55830f7a0cb421f3375c39d2b104e9957d852
Michael Chermside <mcherm@mcherm.com>
2021-12-24 21:04:23 -0500

    solved 41.

--- adventofcode/src/problem_41.rs
+++ adventofcode/src/problem_41.rs
@@ -35,7 +35,7 @@ impl fmt::Display for InputError {
 }
 
 /// Read in the input file.
-fn read_dice_game_file() -> Result<(usize, usize), InputError> {
+fn read_dice_game_file() -> Result<[usize;2], InputError> {
     // --- open file ---
     let filename = "data/2021/day/21/input.txt";
     let file = File::open(filename)?;
@@ -55,12 +55,75 @@ fn read_dice_game_file() -> Result<(usize, usize), InputError> {
     let p2_pos: usize = capture.get(1).unwrap().as_str().parse()?;
 
     // --- return result ---
-    Ok((p1_pos, p2_pos))
+    Ok([p1_pos, p2_pos])
 }
+
+
+struct DeterministicDie {
+    next: usize,
+    times_rolled: usize,
+}
+
+struct GameState {
+    die: DeterministicDie,
+    positions: [usize;2],
+    scores: [usize;2],
+}
+
+
+impl DeterministicDie {
+    fn new() -> Self {
+        DeterministicDie{next: 1, times_rolled: 0}
+    }
+
+    fn roll(&mut self) -> usize {
+        let answer = self.next;
+        self.next += 1;
+        if self.next == 101 {
+            self.next = 1;
+        }
+        self.times_rolled += 1;
+        answer
+    }
+}
+
+impl GameState {
+    fn new(positions: [usize;2]) -> Self {
+        GameState{die: DeterministicDie::new(), positions, scores: [0;2]}
+    }
+
+    fn take_turn(&mut self, player: usize) {
+        let roll_sum = self.die.roll() + self.die.roll() + self.die.roll();
+        self.positions[player] = (self.positions[player] + roll_sum - 1) % 10 + 1;
+        self.scores[player] += self.positions[player];
+    }
+
+    fn game_not_over(&self) -> bool {
+        self.scores[0] < 1000 && self.scores[1] < 1000
+    }
+
+    fn low_score(&self) -> usize {
+        *self.scores.iter().min().unwrap()
+    }
+}
+
 
 fn run() -> Result<(),InputError> {
     let starts = read_dice_game_file()?;
-    println!("starts: ({},{})", starts.0, starts.1);
+    println!("starts: ({},{})", starts[0], starts[1]);
+
+    let mut game = GameState::new(starts);
+    let mut player = 0;
+    while game.game_not_over() {
+        game.take_turn(player);
+        player = (player + 1) % 2;
+    }
+    println!("After the game we had {} low score and {} rolls for a total of {}",
+        game.low_score(),
+        game.die.times_rolled,
+        game.low_score() * game.die.times_rolled
+    );
+
     Ok(())
 }
 
