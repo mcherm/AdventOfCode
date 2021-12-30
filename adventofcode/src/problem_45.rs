611899fb17114c6ad8260614d3cb6dbc5f5cf83f
Michael Chermside <mcherm@mcherm.com>
2021-12-29 23:34:43 -0500

    I now have working code that determines which moves are legal.

--- adventofcode/src/problem_45.rs
+++ adventofcode/src/problem_45.rs
@@ -4,6 +4,12 @@ use std::fs;
 use nom::bytes::complete::tag as nom_tag;
 use nom::sequence::tuple as nom_tuple;
 use nom::branch::alt as nom_alt;
+use Location::{
+    Hall0, Hall1, Hall2, Hall3, Hall4, Hall5, Hall6,
+    FrontOfA, FrontOfB, FrontOfC, FrontOfD,
+    BackOfA, BackOfB, BackOfC, BackOfD
+};
+use AmphipodType::{Amber, Bronze, Copper, Desert};
 
 
 // ======== Reading Input ========
@@ -31,12 +37,12 @@ impl fmt::Display for InputError {
 }
 
 /// Read in the input file.
-fn read_maze_file() -> Result<Positions, InputError> {
+fn read_maze_file() -> Result<Position, InputError> {
     // --- read file ---
     let filename = "data/2021/day/23/input.txt";
     let contents = fs::read_to_string(filename)?;
     // NOTE: I should raise an error, not unwrap but I don't know how.
-    let (rest, positions) = Positions::parse_nom(&contents).unwrap();
+    let (rest, positions) = Position::parse_nom(&contents).unwrap();
     if rest != "" {
         return Err(InputError::InvalidMazeFile);
     }
@@ -58,6 +64,7 @@ enum AmphipodType {
 }
 
 
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 enum Location {
     Hall0 = 0,
     Hall1 = 1,
@@ -77,15 +84,26 @@ enum Location {
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
-struct Positions {
+struct Position {
     slots: [Option<AmphipodType>; Location::NUM_VALUES],
 }
 
+#[allow(dead_code)]
+type Cost = u32;
 
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+struct Move {
+    amph: AmphipodType,
+    from: Location,
+    to: Location,
+}
 
 // ======== Implementations ========
 
 impl AmphipodType {
+    const NUM_VALUES: usize = 4;
+    const ALL: [AmphipodType; AmphipodType::NUM_VALUES] = [Amber, Bronze, Copper, Desert];
+
     /// Reads a field which could be an AmphipodType or a "." for None.
     fn parse_nom(input: &str) -> nom::IResult<&str, Option<Self>> {
         nom_alt((
@@ -95,10 +113,10 @@ impl AmphipodType {
             nom_tag("D"),
             nom_tag("."),
         ))(input).map(|(rest, res)| (rest, match res {
-            "A" => Some(AmphipodType::Amber),
-            "B" => Some(AmphipodType::Bronze),
-            "C" => Some(AmphipodType::Copper),
-            "D" => Some(AmphipodType::Desert),
+            "A" => Some(Amber),
+            "B" => Some(Bronze),
+            "C" => Some(Copper),
+            "D" => Some(Desert),
             "." => None,
             _ => panic!("should never happen")
         }))
@@ -106,10 +124,20 @@ impl AmphipodType {
 
     fn to_str(&self) -> &'static str {
         match self {
-            AmphipodType::Amber => &"A",
-            AmphipodType::Bronze => &"B",
-            AmphipodType::Copper => &"C",
-            AmphipodType::Desert => &"D",
+            Amber => &"A",
+            Bronze => &"B",
+            Copper => &"C",
+            Desert => &"D",
+        }
+    }
+
+    /// Returns the nook index for a given AmphipodType
+    fn nook(&self) -> usize {
+        match self {
+            Amber => 0,
+            Bronze => 1,
+            Copper => 2,
+            Desert => 3,
         }
     }
 }
@@ -123,10 +151,75 @@ impl Display for AmphipodType {
 
 impl Location {
     const NUM_VALUES: usize = 15;
+    const FRONT_SLOTS: [Location; 4] = [FrontOfA, FrontOfB, FrontOfC, FrontOfD];
+    const BACK_SLOTS: [Location; 4] = [BackOfA, BackOfB, BackOfC, BackOfD];
+    #[allow(dead_code)]
+    const HALL_SLOTS: [Location; 7] = [Hall0, Hall1, Hall2, Hall3, Hall4, Hall5, Hall6];
+
+    fn to_str(&self) -> &'static str {
+        match self {
+            Hall0 => "Hall0",
+            Hall1 => "Hall1",
+            Hall2 => "Hall2",
+            Hall3 => "Hall3",
+            Hall4 => "Hall4",
+            Hall5 => "Hall5",
+            Hall6 => "Hall6",
+            FrontOfA => "FrontOfA",
+            FrontOfB => "FrontOfB",
+            FrontOfC => "FrontOfC",
+            FrontOfD => "FrontOfD",
+            BackOfA => "BackOfA",
+            BackOfB => "BackOfB",
+            BackOfC => "BackOfC",
+            BackOfD => "BackOfD",
+        }
+    }
+
+    /// Pass this a nook number and it will return two vectors of Hall
+    /// locations: the first is those reachable by turning left, the
+    /// second is those reachable by turning right. They are in the
+    /// order in which the locations need to be entered.
+    fn hall_from(nook: AmphipodType) -> (Vec<Location>, Vec<Location>) {
+        match nook {
+            Amber => (
+                vec![Hall1, Hall0],
+                vec![Hall2, Hall3, Hall4, Hall5, Hall6]
+            ),
+            Bronze => (
+                vec![Hall2, Hall1, Hall0],
+                vec![Hall3, Hall4, Hall5, Hall6]
+            ),
+            Copper => (
+                vec![Hall3, Hall2, Hall1, Hall0],
+                vec![Hall4, Hall5, Hall6]
+            ),
+            Desert => (
+                vec![Hall4, Hall3, Hall2, Hall1, Hall0],
+                vec![Hall5, Hall6]
+            ),
+        }
+    }
+}
+
+impl Display for Location {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_str())
+    }
 }
 
 
-impl Positions {
+
+
+impl Position {
+    /// Parses a position, assuming it is valid and panicking if it isn't.
+    #[allow(dead_code)]
+    fn parse_good(input: &str) -> Self {
+        let (rest, answer) = Self::parse_nom(input).unwrap();
+        assert!(rest.len() == 0);
+        answer
+    }
+
     fn parse_nom(input: &str) -> nom::IResult<&str, Self> {
         // This tuple was so long we had to break it into 2 separate tuples
         nom_tuple((
@@ -170,11 +263,15 @@ impl Positions {
                 (_, fa, _, fb, _, fc, _, fd, _, ba, _, bb, _, bc, _, bd, _)
             ))| {(
                 rest,
-                Positions{
+                Position {
                     slots: [h0, h1, h2, h3, h4, h5, h6, fa, fb, fc, fd, ba, bb, bc, bd],
                 }
             )}
         )
+    }
+
+    fn at(&self, loc: Location) -> Option<AmphipodType> {
+        self.slots[loc as usize]
     }
     
     fn show_opt_amphipod_at(&self, loc: Location) -> &'static str {
@@ -183,9 +280,93 @@ impl Positions {
             Some(a) => a.to_str(),
         }
     }
+
+    /// Returns a vector of all legal moves from this position.
+    fn legal_moves(&self) -> Vec<Move> {
+        let mut answer = Vec::new();
+
+        // -- Moves out of a nook --
+        for a in AmphipodType::ALL {
+            let front = Location::FRONT_SLOTS[a.nook()];
+            let back = Location::BACK_SLOTS[a.nook()];
+            let from_opt: Option<Location> = match self.at(front) {
+                Some(_) => Some(front),
+                None => match self.at(back) {
+                    Some(_) => Some(back),
+                    None => None
+                },
+            };
+            if let Some(from) = from_opt {
+                let amph: AmphipodType = self.at(from).unwrap();
+                let (left_hall, right_hall) = Location::hall_from(a);
+                for to in left_hall {
+                    match self.at(to) {
+                        Some(_) => break, // no more space on the left
+                        None => answer.push(Move{amph, from, to}),
+                    }
+                }
+                for to in right_hall {
+                    match self.at(to) {
+                        Some(_) => break, // no more space on the right
+                        None => answer.push(Move{amph, from, to}),
+                    }
+                }
+            }
+        }
+
+        // -- Moves into a nook --
+        for a in AmphipodType::ALL {
+            let front = Location::FRONT_SLOTS[a.nook()];
+            let back = Location::BACK_SLOTS[a.nook()];
+            let to_opt: Option<Location> = match self.at(front) {
+                Some(_) => None,
+                None => match self.at(back) {
+                    Some(_) => Some(front),
+                    None => Some(back),
+                },
+            };
+            if let Some(to) = to_opt {
+                let (left_hall, right_hall) = Location::hall_from(a);
+                'left_hall:
+                for from in left_hall {
+                    match self.at(from) {
+                        Some(amph) => {
+                            if amph == a {
+                                answer.push(Move{amph, from, to});
+                            }
+                            break 'left_hall; // no more to the left
+                        },
+                        None => {}, // keep looking to the left
+                    }
+                }
+                for from in right_hall {
+                    match self.at(from) {
+                        Some(amph) => {
+                            if amph == a {
+                                answer.push(Move{amph, from, to});
+                            }
+                            break; // no more to the right
+                        },
+                        None => {}, // keep looking to the right
+                    }
+                }
+            }
+        }
+
+        // -- Return answer --
+        answer
+    }
+
+
+    fn perform(&self, mv: Move) -> Position {
+        let mut slots = self.slots.clone();
+        slots[mv.from as usize] = None;
+        slots[mv.to as usize] = Some(mv.amph);
+        Position{slots}
+    }
 }
 
-impl Display for Positions {
+impl Display for Position {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(
             f,
@@ -196,21 +377,21 @@ impl Display for Positions {
   #{}#{}#{}#{}#
   #########
 ",
-            self.show_opt_amphipod_at(Location::Hall0),
-            self.show_opt_amphipod_at(Location::Hall1),
-            self.show_opt_amphipod_at(Location::Hall2),
-            self.show_opt_amphipod_at(Location::Hall3),
-            self.show_opt_amphipod_at(Location::Hall4),
-            self.show_opt_amphipod_at(Location::Hall5),
-            self.show_opt_amphipod_at(Location::Hall6),
-            self.show_opt_amphipod_at(Location::FrontOfA),
-            self.show_opt_amphipod_at(Location::FrontOfB),
-            self.show_opt_amphipod_at(Location::FrontOfC),
-            self.show_opt_amphipod_at(Location::FrontOfD),
-            self.show_opt_amphipod_at(Location::BackOfA),
-            self.show_opt_amphipod_at(Location::BackOfB),
-            self.show_opt_amphipod_at(Location::BackOfC),
-            self.show_opt_amphipod_at(Location::BackOfD),
+            self.show_opt_amphipod_at(Hall0),
+            self.show_opt_amphipod_at(Hall1),
+            self.show_opt_amphipod_at(Hall2),
+            self.show_opt_amphipod_at(Hall3),
+            self.show_opt_amphipod_at(Hall4),
+            self.show_opt_amphipod_at(Hall5),
+            self.show_opt_amphipod_at(Hall6),
+            self.show_opt_amphipod_at(FrontOfA),
+            self.show_opt_amphipod_at(FrontOfB),
+            self.show_opt_amphipod_at(FrontOfC),
+            self.show_opt_amphipod_at(FrontOfD),
+            self.show_opt_amphipod_at(BackOfA),
+            self.show_opt_amphipod_at(BackOfB),
+            self.show_opt_amphipod_at(BackOfC),
+            self.show_opt_amphipod_at(BackOfD),
         )
     }
 }
@@ -223,8 +404,37 @@ impl Display for Positions {
 
 
 fn run() -> Result<(),InputError> {
-    let positions: Positions = read_maze_file()?;
-    println!("{}", positions);
+    let mut position: Position = read_maze_file()?;
+    println!("{}", position);
+    let legal_moves = position.legal_moves();
+    for mv in legal_moves.iter() {
+        println!("Move: {:?}", mv);
+    }
+
+    position = position.perform(legal_moves[0]);
+    println!();
+    println!("{}", position);
+    let legal_moves = position.legal_moves();
+    for mv in legal_moves.iter() {
+        println!("Move: {:?}", mv);
+    }
+
+    position = position.perform(legal_moves[0]);
+    println!();
+    println!("{}", position);
+    let legal_moves = position.legal_moves();
+    for mv in legal_moves.iter() {
+        println!("Move: {:?}", mv);
+    }
+
+    position = position.perform(legal_moves[0]);
+    println!();
+    println!("{}", position);
+    let legal_moves = position.legal_moves();
+    for mv in legal_moves.iter() {
+        println!("Move: {:?}", mv);
+    }
+
     Ok(())
 }
 
@@ -249,4 +459,24 @@ mod test {
         let _ = read_maze_file().unwrap();
     }
 
+    #[test]
+    fn test_legal_moves_1() {
+        let position = Position::parse_good("#############
+#B..A.C.....#
+###.#.#B#D###
+  #.#D#C#A#
+  #########\n");
+        assert_eq!(
+            vec![
+                Move{amph: Bronze, from: FrontOfC, to: Hall4},
+                Move{amph: Bronze, from: FrontOfC, to: Hall5},
+                Move{amph: Bronze, from: FrontOfC, to: Hall6},
+                Move{amph: Desert, from: FrontOfD, to: Hall4},
+                Move{amph: Desert, from: FrontOfD, to: Hall5},
+                Move{amph: Desert, from: FrontOfD, to: Hall6},
+                Move{amph: Amber, from: Hall2, to: BackOfA},
+            ],
+            position.legal_moves()
+        );
+    }
 }
