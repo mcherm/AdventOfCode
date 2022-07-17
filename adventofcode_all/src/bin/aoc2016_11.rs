b00a9bf83a06ff3a1c4e3630fdbb4f6ff47d8516
Michael Chermside <mcherm@mcherm.com>
2022-07-17 08:54:40 -0400

    Solved aoc2016_11 part 1.

--- adventofcode_all/src/bin/aoc2016_11.rs
+++ adventofcode_all/src/bin/aoc2016_11.rs
@@ -1,15 +1,76 @@
 
 extern crate anyhow;
 
+/// This module provides a SortedVec which is a wrapper around Vec that guarantees
+/// the Vec will be kept in sorted order. The wrapper only bothers to implement
+/// the specific features of Vec that we're using in this file. We have this
+/// because we want a Vec of items for which order is not significant for equality
+/// testing or hashing.
+mod sorted_vec {
+    use core::hash::Hash;
+    use std::ops::{Deref, DerefMut};
+
+    #[derive(Debug, Hash, Eq, PartialEq, Clone)]
+    pub struct SortedVec<T>(Vec<T>)
+        where T: Hash + Ord;
+
+    impl<T: Hash + Ord> SortedVec<T> {
+        pub fn new() -> Self {
+            SortedVec(Vec::new())
+        }
+
+        pub fn push(&mut self, value: T) {
+            let insert_pos = self.0.binary_search(&value).unwrap_or_else(|e| e);
+            self.0.insert(insert_pos, value);
+        }
+
+        pub fn retain<F>(&mut self, f: F) where F: FnMut(&T) -> bool {
+            self.0.retain(f);
+        }
+    }
+
+    impl<T: Hash + Ord> Deref for SortedVec<T> {
+        type Target = [T];
+
+        fn deref(&self) -> &Self::Target {
+            self.0.deref()
+        }
+
+    }
+
+    impl<T: Hash + Ord> DerefMut for SortedVec<T> {
+        fn deref_mut(&mut self) -> &mut Self::Target {
+            self.0.deref_mut()
+        }
+    }
+
+    impl<T: Hash + Ord> IntoIterator for SortedVec<T> {
+        type Item = T;
+        type IntoIter = std::vec::IntoIter<Self::Item>;
+
+        fn into_iter(self) -> Self::IntoIter {
+            self.0.into_iter()
+        }
+    }
+}
+
+
+
+use sorted_vec::SortedVec;
+use std::collections::HashSet;
+use std::collections::VecDeque;
+use std::fmt::{Display, Formatter};
 use std::fs;
 use anyhow::Error;
+
+
 
 use nom::{
     IResult,
     branch::alt,
     bytes::complete::tag,
     character::complete::{alpha1, newline},
-    combinator::{value, map},
+    combinator::{value, map, opt},
     multi::{many0, separated_list1},
     sequence::{terminated, tuple},
 };
@@ -26,10 +87,21 @@ fn input() -> Result<Vec<FloorDescription>, Error> {
 
 
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Ord, PartialOrd)]
 pub enum Item {
     Generator(String),
     Microchip(String),
+}
+
+
+impl Display for Item {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let (name_abbrev, type_code) = match self {
+            Item::Generator(s) => (s.chars().nth(0).unwrap_or(' '), 'G'),
+            Item::Microchip(s) => (s.chars().nth(0).unwrap_or(' '), 'M'),
+        };
+        write!(f, "{}{}", name_abbrev, type_code)
+    }
 }
 
 impl Item {
@@ -77,10 +149,11 @@ impl Item {
         map(
             tuple((
                 separated_list1( tag(", "), Self::parse ),
-                tag(", and "),
+                opt(tag(",")),
+                tag(" and "),
                 Self::parse,
             )),
-            |(mut items, _, last_item)| {
+            |(mut items, _, _, last_item)| {
                 items.push(last_item);
                 items
             }
@@ -145,12 +218,191 @@ impl FloorDescription {
     }
 }
 
+#[derive(Hash, Eq, PartialEq, Clone)]
+struct State {
+    elevator: usize,
+    data: Vec<SortedVec<Item>>
+}
+
+impl State {
+    /// Initialize a State from the descriptions
+    fn from_descriptions(floor_descriptions: &Vec<FloorDescription>) -> Self {
+        let mut data = Vec::new();
+        let num_floors = floor_descriptions.len();
+        for floor in 0..num_floors {
+            for floor_description in floor_descriptions {
+                if usize::from(floor_description.floor_num) == floor + 1 {
+                    let mut floor_items = SortedVec::new();
+                    for item in &floor_description.items {
+                        floor_items.push(item.clone())
+                    }
+                    data.push(floor_items);
+                    break;
+                }
+            }
+        }
+        let elevator = 0;
+        State{elevator, data}
+    }
+
+    /// Tests whether a state is legal.
+    fn is_legal(&self) -> bool {
+        for floor_items in &self.data {
+            for item in floor_items.iter() {
+                if let Item::Microchip(name) = item {
+                    let mut microchip_plugged_in = false;
+                    let mut other_generator_present = false;
+                    for other_item in floor_items.iter() {
+                        if let Item::Generator(other_name) = other_item {
+                            if *name == *other_name {
+                                microchip_plugged_in = true;
+                            } else {
+                                other_generator_present = true;
+                            }
+                        }
+                    }
+                    if other_generator_present && !microchip_plugged_in {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
+    }
+
+    /// Returns true if the object has all items on the top floor (so it wins); false
+    /// if not.
+    fn winning(&self) -> bool {
+        for (floor_num, floor_items) in self.data.iter().enumerate() {
+            if floor_num < self.data.len() - 1 {
+                if floor_items.len() > 0 {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+
+    /// Returns a new State identical to this, but the given item is removed
+    /// from the floor with the current elevator and put on the floor
+    /// new_elevator, and the elevator is moved there also.
+    fn move_item(&self, new_elevator: usize, moved_items: Vec<&Item>) -> State {
+        let mut new_data = Vec::new();
+        for (floor_num, floor_items) in self.data.iter().enumerate() {
+            new_data.push(match floor_num {
+                _ if floor_num == self.elevator => {
+                    // leave off items
+                    let mut new_items = floor_items.clone();
+                    new_items.retain(|i| !moved_items.contains(&i));
+                    new_items
+                },
+                _ if floor_num == new_elevator => {
+                    // add item
+                    let mut new_items = floor_items.clone();
+                    for moved_item in moved_items.iter() {
+                        new_items.push((*moved_item).clone())
+                    }
+                    new_items
+                },
+                _ => {
+                    // exactly as-is
+                    floor_items.clone()
+                },
+            });
+        }
+        State{elevator: new_elevator, data: new_data}
+    }
+
+    /// Returns a list of all possible next states (legal or not).
+    fn possible_next_states(&self) -> Vec<State> {
+        let mut answer = Vec::new();
+        let floors = self.data.len();
+        let elevator_floor_items: &SortedVec<Item> = self.data.get(self.elevator).unwrap();
+
+        let mut possible_new_floors = Vec::new();
+        if self.elevator < floors - 1 {
+            // --- We can try things that move the elevator up ---
+            possible_new_floors.push(self.elevator + 1);
+        }
+        if self.elevator > 0 {
+            // --- We can try things that move the elevator down ---
+            possible_new_floors.push(self.elevator - 1);
+        }
+
+        for new_floor in possible_new_floors {
+            // -- Try two things in the elevator --
+            if elevator_floor_items.len() >= 2 {
+                for p1 in 0..(elevator_floor_items.len() - 1) {
+                    for p2 in (p1 + 1)..elevator_floor_items.len() {
+                        let new_floor_items = self.move_item(new_floor, vec![
+                            elevator_floor_items.get(p1).unwrap(),
+                            elevator_floor_items.get(p2).unwrap(),
+                        ]);
+                        answer.push(new_floor_items);
+                    }
+                }
+            }
+            // -- Try just one thing in the elevator --
+            for item in elevator_floor_items.iter() {
+                let new_floor_items = self.move_item(new_floor, vec![&item]);
+                answer.push(new_floor_items);
+            }
+        }
+        answer
+    }
+}
+
+impl Display for State {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for (floor_num, floor_items) in self.data.iter().enumerate().rev() {
+            write!(f, "F{} {}", floor_num + 1, if self.elevator == floor_num {'E'} else {' '})?;
+            for item in floor_items.iter() {
+                write!(f, " {}", item)?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
+
+fn explore_states(initial_state: State) {
+    if initial_state.winning() {
+        println!("That starting state is already a winner.");
+        return;
+    }
+
+    let mut visited_states: HashSet<State> = HashSet::new();
+    visited_states.insert(initial_state.clone());
+    let mut available_states: VecDeque<(usize, State)> = VecDeque::new(); // (num_steps, state)
+    available_states.push_back((0,initial_state));
+
+    loop {
+        let (steps, from_state) = available_states.pop_front().expect("There is no way to find a solution.");
+        for s in from_state.possible_next_states() {
+            if !visited_states.contains(&s) && s.is_legal() {
+                if s.winning() {
+                    println!("**** FOUND A WINNER ****");
+                    println!("In {} steps:", steps + 1);
+                    println!("{}", s);
+                    return;
+                }
+                println!("In {} steps:", steps + 1);
+                println!("{}", s);
+                visited_states.insert(s.clone());
+                available_states.push_back((steps + 1, s));
+                println!("Tried {} legal states.", visited_states.len());
+            }
+        }
+    }
+}
+
 
 fn part_a(floor_descriptions: &Vec<FloorDescription>) {
     println!("\nPart a:");
-    for floor_description in floor_descriptions {
-        println!("{:?}", floor_description);
-    }
+    let initial_state = State::from_descriptions(floor_descriptions);
+    println!("{}", initial_state);
+    explore_states(initial_state);
 }
 
 
