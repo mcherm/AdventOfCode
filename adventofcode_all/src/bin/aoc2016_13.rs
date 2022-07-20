8665a103438f3b556d7cc93555695f33cece46dd
Michael Chermside <mcherm@mcherm.com>
2022-07-20 13:45:01 -0400

    Solved aoc2016_13 part 1.

--- adventofcode_all/src/bin/aoc2016_13.rs
+++ adventofcode_all/src/bin/aoc2016_13.rs
@@ -4,6 +4,8 @@ extern crate anyhow;
 use std::fs;
 use anyhow::Error;
 use std::collections::HashSet;
+use std::collections::VecDeque;
+use std::collections::HashMap;
 
 
 fn input() -> Result<usize, Error> {
@@ -91,8 +93,10 @@ fn print_grid(fav: usize, path: &Path) {
     let (max_x, max_y) = max_point([START, DESTINATION, path.max_point()]);
     for y in 0..(max_y + PADDING) {
         for x in 0..(max_x + PADDING) {
-            let c = if (x,y) == DESTINATION || (x,y) == START {
+            let c = if (x,y) == DESTINATION  {
                 "X"
+            } else if (x,y) == START {
+                "O"
             } else if path.contains(&(x,y)) {
                 "*"
             } else if is_open(fav, x, y) {
@@ -145,13 +149,91 @@ fn explore_grid(fav: usize) -> Path {
     path
 }
 
+type Point = (usize,usize);
+
+#[derive(Copy, Clone)]
+struct SolutionStep {
+    steps: usize,
+    prev: Option<Point>
+}
+
+impl SolutionStep {
+    fn make_first() -> Self {
+        SolutionStep{steps: 0, prev: None}
+    }
+
+    fn make(steps: usize, prev_p: Point) -> Self {
+        SolutionStep{steps, prev: Some(prev_p)}
+    }
+}
+
+/// This will find (one of) the shortest solution(s) if there is one. If there is no solution
+/// it will either run forever or panic.
+fn explore_grid_optimal(fav: usize) -> Path {
+    let mut visited: HashMap<Point,SolutionStep> = HashMap::new(); // p -> (shortest_steps, prev_p)
+    let mut tips: VecDeque<(Point, SolutionStep)> = VecDeque::new(); // queue of (p, prev_p)
+    let mut best_solution: Option<SolutionStep> = None;
+
+    tips.push_back((DESTINATION, SolutionStep::make_first()));
+    loop {
+        let (p, prev_step) = match tips.pop_front() {
+            None => break, // We got through everything worth looking at, so we can exit the loop
+            Some(next_tip) => next_tip,
+        };
+        let new_steps = prev_step.steps + 1;
+        if best_solution.is_some() && best_solution.unwrap().steps <= new_steps {
+            continue; // this can't beat the best we've found so don't bother to look at it
+        }
+        if let Some(visited_step) = visited.get(&p) {
+            if visited_step.steps <= new_steps {
+                continue; // already been here in = or < steps, so don't bother to look at it
+            }
+        }
+        // -- we have NOT been here in this few steps before, so we'll try it --
+        visited.insert(p, prev_step);
+        // -- Maybe we've solved it --
+        if p == START {
+            match best_solution {
+                None => {
+                    // -- this is the first solution to be discovered --
+                    best_solution = Some(SolutionStep::make(new_steps, p))
+                },
+                Some(SolutionStep{steps, ..}) if new_steps < steps => {
+                    // -- this is better than the previous best solution --
+                    best_solution = Some(SolutionStep::make(new_steps, p))
+                },
+                _ => {}, // It's NOT a new best solution
+            }
+        } else {
+            // -- not solved, let's go ahead and consider the neighbors of p
+            for neighbor in get_neighbors(fav, p) {
+                tips.push_back((neighbor, SolutionStep::make(new_steps, p)));
+            }
+        }
+    }
+
+    match best_solution {
+        None => panic!("No solution can be found."),
+        Some(SolutionStep{steps, prev}) => {
+            // -- collect the points --
+            let mut path = Path::new();
+            let mut opt_p = prev;
+            while opt_p.is_some() {
+                path.push(opt_p.unwrap());
+                opt_p = visited.get(&opt_p.unwrap()).unwrap().prev;
+            }
+            path
+        }
+    }
+}
+
 
 
 
 fn part_a(fav: &usize) {
     println!("\nPart a:");
 
-    let path = explore_grid(*fav);
+    let path = explore_grid_optimal(*fav);
     print_grid(*fav, &path);
     println!();
     println!("It takes at least {} steps to complete it.", path.steps())
