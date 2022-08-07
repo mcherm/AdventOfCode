444e9829559042fc9336909d31b934a287c4f8eb
Michael Chermside <mcherm@mcherm.com>
2022-08-07 09:07:11 -0400

    Implement a basic A* search (still needs lots of work).

--- adventofcode_all/src/bin/aoc2016_22.rs
+++ adventofcode_all/src/bin/aoc2016_22.rs
@@ -3,7 +3,7 @@ extern crate anyhow;
 
 use std::fs;
 use anyhow::Error;
-use std::cmp::max;
+use std::cmp::{max, Ordering};
 use std::collections::{HashMap, VecDeque};
 use std::collections::hash_map::DefaultHasher;
 use std::fmt::{Display, Formatter};
@@ -76,6 +76,16 @@ struct State {
     nodes: HashMap<Coord,NodeSpace>,
     goal_data: Coord,
     avail_steps: Vec<Step>,
+}
+
+
+/// This is what we insert into the queue while doing an A* search. It has a State and the
+/// number of steps it took to get there. They are sortable (because the queue is kept
+/// sorted) and the sort order is by step_count + state.min_steps_to_win()
+struct StateToConsider {
+    state: State, // the state we will consider
+    step_count: usize, // the number of steps it took to arrive at that state
+    prev: Option<(State, Step)>, // the previous state and the step from it, or None if this is the FIRST state.
 }
 
 
@@ -275,6 +285,13 @@ impl State {
         self.nodes.iter().map(|x| (x.0.clone(), x.1.clone())).sorted().collect()
     }
 
+    /// Returns the minimum possible steps needed to win, which I'll define as the taxicab
+    /// distance to the goal. [NOTE: Would it be better if we also considered empty spaces
+    /// between here and the goal?]
+    fn min_steps_to_win(&self) -> usize {
+        self.goal_data.0 + self.goal_data.1
+    }
+
     /// Returns the new State achieved by performing this step (which ought to be one of the
     /// valid Steps for this State).
     fn enact_step(&self, step: &Step, size: Coord) -> State {
@@ -360,6 +377,98 @@ impl Display for State {
 }
 
 
+impl StateToConsider {
+    fn sort_score(&self) -> usize {
+        self.step_count + self.state.min_steps_to_win()
+    }
+}
+
+
+
+
+
+fn find_winning_steps(grid: &Grid) -> Option<Vec<Step>> {
+    let initial_state = grid.get_initial_state();
+    println!("State: {:}", initial_state);
+
+    // visited_from maps from a state (which we have considered and explored its neighbors) to how
+    // we got there: (prev_state, prev_step, step_count).
+    let mut visited_from: HashMap<State, Option<(State, Step, usize)>> = HashMap::new();
+
+    // queue is a collection of states we will consider. What we store is
+    //   StateToConsider. The queue is kept sorted by sort_score()
+    let mut queue: VecDeque<StateToConsider> = VecDeque::new();
+    queue.push_back(StateToConsider{state: initial_state, step_count: 0, prev: None });
+
+    // let mut winning_steps: Option<Vec<Step>> = None;
+
+    loop {
+        match queue.pop_front() {
+            None => {
+                return None; // we ran out of places to go. Guess it's not solvable!
+            }
+            Some(StateToConsider{state, step_count, prev}) => {
+                // -- mark that we have (or now will!) visited this one --
+                let how_we_got_here = match prev {
+                    None => None,
+                    Some((prev_state, prev_step)) => Some((prev_state, prev_step, step_count))
+                }; // FIXME: after it works, see whether I can store the whole optional tuple and simplify this line
+                visited_from.insert(state.clone(), how_we_got_here);
+
+                // -- try each step from here --
+                for step in &state.avail_steps {
+                    let next_state = state.enact_step(step, grid.size);
+                    let next_steps = step_count + 1;
+
+                    // -- maybe we've already been to this one --
+                    let earlier_visit = visited_from.get(&next_state);
+                    // -- decide whether to put next_state onto the queue... --
+                    let try_next_state = match earlier_visit {
+                        None => true, // never seen it, certainly want to try it out
+                        Some(None) => false, // the earlier visit was our starting position
+                        Some(Some((_, _, earlier_step_count))) => {
+                            match earlier_step_count.cmp(&next_steps) {
+                                Ordering::Greater => panic!("Found faster way to a visited site."), // FIXME: Maybe this is legal?
+                                Ordering::Equal => false, // been here same distance; don't try it
+                                Ordering::Less => false, // been here better distance; don't try it
+                            }
+                        }
+                    };
+
+                    if try_next_state {
+                        if next_state.is_winning() {
+                            println!("SOLVED!! {}", next_state);
+                            let winning_steps = Some({
+                                let mut steps: Vec<Step> = Vec::new();
+                                steps.push(*step);
+                                let mut state_var: &State = &state;
+                                while let Some((prev_state, prev_step, _)) = visited_from.get(&state_var).unwrap() {
+                                    steps.push((*prev_step).clone());
+                                    state_var = prev_state;
+                                }
+                                steps.reverse();
+                                steps
+                            });
+                            return winning_steps
+                        } else {
+                            // -- Actually add this to the queue --
+                            let to_insert = StateToConsider{
+                                state: next_state,
+                                step_count: step_count + 1,
+                                prev: Some((state.clone(), *step))
+                            };
+                            let insert_idx = queue.partition_point(|x| x.sort_score() < to_insert.sort_score());
+                            queue.insert(insert_idx, to_insert);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
 fn part_a(grid: &Grid) {
     println!("\nPart a:");
     let pair_count = grid.count_viable_pairs();
@@ -370,47 +479,8 @@ fn part_a(grid: &Grid) {
 
 fn part_b(grid: &Grid) {
     println!("\nPart b:");
-    let initial_state = grid.get_initial_state();
-    println!("State: {:}", initial_state);
-    // visited maps from a state (which we have visited) to how we got there: a prev_state and step
-    let mut visited: HashMap<State,Option<(State,Step)>> = HashMap::new();
-    visited.insert(initial_state.clone(), None);
-    let mut queue: VecDeque<State> = VecDeque::new();
-    queue.push_back(initial_state);
-    let mut winning_steps: Option<Vec<Step>> = None;
 
-    loop {
-        match queue.pop_front() {
-            None => {
-                break;
-            }
-            Some(state) => {
-                for step in &state.avail_steps {
-                    let next_state = state.enact_step(step, grid.size);
-                    if !visited.contains_key(&next_state) {
-                        if next_state.is_winning() {
-                            println!("SOLVED!! {}", next_state);
-                            winning_steps = Some({
-                                let mut steps: Vec<Step> = Vec::new();
-                                steps.push(*step);
-                                let mut state_x: &State = &state;
-                                while let Some((prev_state, prev_step)) = visited.get(&state_x).unwrap() {
-                                    steps.push((*prev_step).clone());
-                                    state_x = prev_state;
-                                }
-                                steps.reverse();
-                                steps
-                            });
-                            queue.clear(); // so we will exit the bigger loop
-                            break;
-                        }
-                        visited.insert(next_state.clone(), Some((state.clone(),*step)));
-                        queue.push_back(next_state);
-                    }
-                }
-            }
-        }
-    }
+    let winning_steps = find_winning_steps(grid);
     match winning_steps {
         None => println!("Could not find a solution."),
         Some(steps) => {
@@ -425,6 +495,7 @@ fn part_b(grid: &Grid) {
     }
     println!("Done.");
 }
+
 
 
 fn main() -> Result<(), Error> {
@@ -443,4 +514,16 @@ fn main() -> Result<(), Error> {
 mod tests {
     use super::*;
 
+    #[test]
+    fn test_vec_deque() {
+        let vd: VecDeque<(char,i32)> = VecDeque::from([('a',1), ('b',2), ('c',3), ('d',3), ('e',4), ('f',6)]);
+        match vd.binary_search_by_key(&5, |x| x.1) {
+            Ok(x) => {
+                println!("FOUND x = {}", x);
+            }
+            Err(x) => {
+                println!("NOPE x = {}", x);
+            }
+        }
+    }
 }
