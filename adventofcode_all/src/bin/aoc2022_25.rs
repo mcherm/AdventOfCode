30a7bad43b2df76a11831eddd7b4b1ca4326c1c8
Michael Chermside <mcherm@mcherm.com>
2023-03-04 16:53:08 -0500

    Finished aoc2022_25. Which finishes all of year 2022.

--- adventofcode_all/src/bin/aoc2022_25.rs
+++ adventofcode_all/src/bin/aoc2022_25.rs
@@ -37,7 +37,7 @@ mod parse {
         MinusTwo, MinusOne, Zero, One, Two
     }
 
-    #[derive(Debug)]
+    #[derive(Debug, Eq, PartialEq)]
     pub struct Snafu {
         pub digits: Vec<Digit>
     }
@@ -76,6 +76,73 @@ mod compute {
     use std::fmt::{Display, Formatter};
     use std::ops::Add;
     use crate::parse::{Digit, Snafu};
+    use itertools::EitherOrBoth::{Both, Right};
+    use itertools::{EitherOrBoth, Itertools};
+
+    #[derive(Debug, Copy, Clone)]
+    enum Overflow {
+        OverMinus,
+        OverZero,
+        OverPlus,
+    }
+
+    impl Overflow {
+        fn as_num(&self) -> i8 {
+            use Overflow::*;
+            match self {
+                OverMinus => -1,
+                OverZero => 0,
+                OverPlus => 1,
+            }
+        }
+    }
+
+    impl Display for Overflow {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}", match self {
+                Overflow::OverMinus => '-',
+                Overflow::OverZero => '0',
+                Overflow::OverPlus => '+',
+            })
+        }
+    }
+
+    impl Digit {
+
+        fn as_num(&self) -> i8 {
+            use Digit::*;
+            match self {
+                MinusTwo => -2,
+                MinusOne => -1,
+                Zero => 0,
+                One => 1,
+                Two => 2,
+            }
+        }
+    }
+
+    /// This sums individual digits. It takes in a pair of digits and an overflow
+    /// and returns the output digit and overflow.
+    fn sum_digit(d1: Digit, d2: Digit, overflow: Overflow) -> (Digit, Overflow) {
+        use Digit::*;
+        use Overflow::*;
+        match d1.as_num() + d2.as_num() + overflow.as_num() {
+            -5 => (Zero    , OverMinus),
+            -4 => (One     , OverMinus),
+            -3 => (Two     , OverMinus),
+            -2 => (MinusTwo, OverZero),
+            -1 => (MinusOne, OverZero),
+            0  => (Zero    , OverZero),
+            1  => (One     , OverZero),
+            2  => (Two     , OverZero),
+            3  => (MinusTwo, OverPlus),
+            4  => (MinusOne, OverPlus),
+            5  => (Zero, OverPlus),
+            _ => panic!("Add out of bounds."),
+        }
+    }
+
+
 
     impl Snafu {
         pub fn zero() -> Self {
@@ -108,13 +175,74 @@ mod compute {
         type Output = Self;
 
         fn add(self, rhs: &Snafu) -> Self::Output {
-            for d in self.digits.iter().rev() {
-                println!("{}", d);
+            let mut overflow = Overflow::OverZero;
+
+            // A bunch of work to loop through the pairs of digits from least significant to
+            // greatest, left-padding with zeros to make them the same length.
+            let left_digits = self.digits.iter().rev();
+            let right_digits = rhs.digits.iter().rev();
+            let mut digits = left_digits.zip_longest(right_digits)
+                .map(|digit_pair| match digit_pair {
+                    Both(x, y) => (x,y),
+                    EitherOrBoth::Left(x) => (x, &Digit::Zero),
+                    Right(y) => (&Digit::Zero, y)
+                })
+                .map(|(d1, d2)| {
+                    let (d_sum, new_overflow) = sum_digit(*d1, *d2, overflow);
+                    overflow = new_overflow;
+                    d_sum
+                })
+                .collect_vec();
+
+            // add a "leading" (trailing) digit if we had overflow
+            match overflow {
+                Overflow::OverZero => {},
+                Overflow::OverMinus => {
+                    digits.push(Digit::MinusOne)
+                },
+                Overflow::OverPlus => {
+                    digits.push(Digit::One)
+                },
             }
-            Snafu::zero() // FIXME: Just a placeholder until it works.
+            digits.reverse(); // but we've built it backward, so fix that
+            Snafu{digits}
         }
     }
 
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use anyhow::anyhow;
+
+        fn read_snafu(s: &'static str) -> Result<Snafu,anyhow::Error> {
+            match Snafu::parse(s)? {
+                ("", x) => Ok(x),
+                (_, _) => Err(anyhow!("Did not use the whole input")),
+            }
+        }
+
+        #[test]
+        fn test_add() -> Result<(), anyhow::Error>{
+            let data = [
+                ("2", "-", "1"),
+                ("-", "-" , "="),
+                ("111", "1" , "112"),
+                ("=2", "-" , "=1"),
+                ("2222", "1", "1===="),
+                ("2", "1", "1="),
+                ("1=-0-2", "12111", "1-111="),
+            ];
+            for (s1, s2, answer) in data {
+                let v1 = read_snafu(s1)?;
+                let v2 = read_snafu(s2)?;
+                let expect = read_snafu(answer)?;
+                let sum = v1 + &v2;
+                assert_eq!(sum, expect);
+            }
+            Ok(())
+        }
+    }
 }
 
 // ======= Part 2 Compute =======
@@ -134,6 +262,7 @@ fn part_a(snafus: &Vec<Snafu>) {
 
 fn part_b(_input: &Vec<Snafu>) {
     println!("\nPart b:");
+    println!("Nothing to do.")
 }
 
 
