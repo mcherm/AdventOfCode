9172462a5f2f3b250700672d493dea3f87739feb
Michael Chermside <mcherm@mcherm.com>
2023-12-08 08:42:48 -0500

    Finished aoc2023_07.

--- adventofcode_all/src/bin/aoc2023_07.rs
+++ adventofcode_all/src/bin/aoc2023_07.rs
@@ -2,23 +2,32 @@ use std::cmp::Ordering;
 use std::fmt::{Display, Formatter};
 use anyhow;
 use itertools::Itertools;
+use std::collections::HashSet;
 
 
 // ======= Parsing =======
 
 const CARD_CHARS: &str = "AKQJT98765432";
+const JOKER_CARD_CHARS: &str = "AKQT98765432J";
 
 type Num = u32;
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone, Hash)]
 struct Card {
     c: char,
 }
 
 #[derive(Debug)]
-pub struct Hand {
+pub struct NormalHand {
     cards: [Card; 5],
     bid: Num,
+}
+
+trait Hand: Ord + Display {
+    fn bid(&self) -> Num;
+    fn count(&self, c: char) -> usize;
+    fn hand_type(&self) -> HandType;
+    fn sorting_tuple(&self) -> (HandType, u8, u8, u8, u8, u8);
 }
 
 
@@ -33,7 +42,7 @@ impl Card {
 
 mod parse {
     use std::fs;
-    use super::{Hand, Card};
+    use super::{NormalHand, Card};
     use nom;
     use nom::{
         IResult,
@@ -42,9 +51,9 @@ mod parse {
     use nom::character::complete::u32 as nom_num;
 
 
-    pub fn input<'a>() -> Result<Vec<Hand>, anyhow::Error> {
+    pub fn input<'a>() -> Result<Vec<NormalHand>, anyhow::Error> {
         let s = fs::read_to_string("input/2023/input_07.txt")?;
-        match Hand::parse_list(&s) {
+        match NormalHand::parse_list(&s) {
             Ok(("", x)) => Ok(x),
             Ok((s, _)) => panic!("Extra input starting at {}", s),
             Err(_) => panic!("Invalid input"),
@@ -60,7 +69,7 @@ mod parse {
         }
     }
 
-    impl Hand {
+    impl NormalHand {
         fn parse<'a>(input: &'a str) -> IResult<&'a str, Self> {
             nom::combinator::map(
                 nom::sequence::tuple((
@@ -72,7 +81,7 @@ mod parse {
                     tag(" "),
                     nom_num,
                 )),
-                |(a,b,c,d,e,_,bid)| Hand{cards: [a,b,c,d,e], bid}
+                |(a,b,c,d,e,_,bid)| NormalHand{cards: [a,b,c,d,e], bid}
             )(input)
         }
 
@@ -108,6 +117,11 @@ impl Card {
     fn value(&self) -> u8 {
         14 - (CARD_CHARS.find(self.c).unwrap() as u8)
     }
+
+    /// Returns a number which can be used to sort the cards assuming J is a joker.
+    fn joker_value(&self) -> u8 {
+        14 - (JOKER_CARD_CHARS.find(self.c).unwrap() as u8)
+    }
 }
 
 impl HandType {
@@ -132,8 +146,18 @@ impl PartialOrd for HandType {
     }
 }
 
+impl Ord for HandType {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
 
-impl Hand {
+
+impl Hand for NormalHand {
+    fn bid(&self) -> Num {
+        self.bid
+    }
+
     /// Counts how many Cards with c appear in the hand.
     fn count(&self, c: char) -> usize {
         self.cards.iter()
@@ -175,22 +199,35 @@ impl Hand {
     }
 }
 
+impl NormalHand {
 
-impl PartialEq for Hand {
+    /// Returns this same hand, but with all instances of old_card replaced with new_card.
+    fn substitute(&self, old_card: Card, new_card: Card) -> Self {
+        let cards: [Card; 5] = self.cards.iter()
+            .map(|card| if *card == old_card {new_card} else {*card}) // replace that one
+            .collect_vec()
+            .try_into()
+            .unwrap();
+        let bid = self.bid;
+        NormalHand{cards, bid}
+    }
+}
+
+impl PartialEq for NormalHand {
     fn eq(&self, other: &Self) -> bool {
         self.cards == other.cards
     }
 }
 
-impl Eq for Hand {}
+impl Eq for NormalHand {}
 
-impl PartialOrd for Hand {
+impl PartialOrd for NormalHand {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         self.sorting_tuple().partial_cmp(&other.sorting_tuple())
     }
 }
 
-impl Ord for Hand {
+impl Ord for NormalHand {
     fn cmp(&self, other: &Self) -> Ordering {
         self.partial_cmp(&other).unwrap()
     }
@@ -202,29 +239,103 @@ impl Display for Card {
     }
 }
 
-impl Display for Hand {
+impl Display for NormalHand {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}{}{}{}{}", self.cards[0], self.cards[1], self.cards[2], self.cards[3], self.cards[4])
     }
 }
 
+impl<'a> Display for JokerHand<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+
+/// A wrapper for Hand that sorts differently, because it treats "J" as a joker.
+struct JokerHand<'a>(&'a NormalHand);
+
+
+const JOKER: Card = Card{c: 'J'};
+
+impl<'a> Hand for JokerHand<'a> {
+    fn bid(&self) -> Num { self.0.bid() }
+    fn count(&self, c: char) -> usize { self.0.count(c) }
+
+    fn hand_type(&self) -> HandType {
+        let cards_in_use: HashSet<Card> = self.0.cards.iter().map(|card| card.clone()).collect();
+        if cards_in_use.contains(&JOKER) {
+            // -- It has one-or-more joker, so we'll need to try the possible combinations ---
+            // NOTE: Because hands with more matches always beat hands with fewer matches
+            //   we only need to try replacing ALL jokers with each other card type that appears
+            //   somewhere in the hand. That's only a few combinations to try for each hand.
+            cards_in_use.iter()
+                .filter(|card| **card != JOKER)
+                .map(|card| self.0.substitute(JOKER, *card).hand_type())
+                .max()
+                .unwrap_or_else(|| {
+                    assert!(self.0.cards == [JOKER;5]); // I think this can only happen with a hand of all Jokers
+                    HandType::FiveOfAKind
+                })
+        } else {
+            self.0.hand_type()
+        }
+    }
+
+    fn sorting_tuple(&self) -> (HandType, u8, u8, u8, u8, u8) {
+        (
+            self.hand_type(),
+            self.0.cards[0].joker_value(),
+            self.0.cards[1].joker_value(),
+            self.0.cards[2].joker_value(),
+            self.0.cards[3].joker_value(),
+            self.0.cards[4].joker_value(),
+        )
+    }
+}
+
+impl<'a> PartialEq for JokerHand<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl<'a> Eq for JokerHand<'a> {}
+
+impl<'a> PartialOrd for JokerHand<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.sorting_tuple().partial_cmp(&other.sorting_tuple())
+    }
+}
+
+impl<'a> Ord for JokerHand<'a> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(&other).unwrap()
+    }
+}
 
 // ======= main() =======
 
 
-fn part_a(data: &Vec<Hand>) {
+fn part_a(data: &Vec<NormalHand>) {
     println!("\nPart a:");
-    let hands: Vec<&Hand> = data.iter().sorted().collect();
-    let total_win: Num = hands.iter()
+    let hands: Vec<&NormalHand> = data.iter().sorted().collect();
+    let total_win: Num = hands.into_iter()
         .enumerate()
-        .map(|(i,hand): (usize,&&Hand)| ((i as Num) + 1) * hand.bid)
+        .map(|(i,hand): (usize,&NormalHand)| ((i as Num) + 1) * hand.bid())
         .sum();
     println!("Total winnings: {}", total_win);
 }
 
 
-fn part_b(_data: &Vec<Hand>) {
+fn part_b(data: &Vec<NormalHand>) {
     println!("\nPart b:");
+    let hands: Vec<JokerHand> = data.iter().map(|h| JokerHand(h)).sorted().collect();
+    let total_win: Num = hands.into_iter()
+        .enumerate()
+        .map(|(i,hand): (usize,JokerHand)| ((i as Num) + 1) * hand.bid())
+        .sum();
+    println!("Total winnings with jokers: {}", total_win);
 }
 
 
